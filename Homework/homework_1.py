cc1e2a234c390097e8f45dceadda1624f93fc512
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-04-13 00:16:57 +0000

    Update: Finishing Homework 2 from IWI131. I need to add the testing cases to the footer.

--- Homework/homework_1.py
+++ Homework/homework_1.py
@@ -1,47 +1,39 @@
 # Solicitando los datos de "alimento", para almacenarlos en variables.
-nevarro_nummies = float(input("Nevarro Nummies consumidad (en unidades): "))
+nevarro_nummies = float(input("Nevarro Nummies consumidas (en unidades): "))
 space_soup = float(input("Space Soup consumida (en [ml]): "))
 carne_de_rana = float(input("Carne de rana consumida (en [g]): "))
 
 # Nevarro Nummies, por unidad de 11 [g]
-nevarro_nummies_grasa = 1.90 # [g]
-nevarro_nummies_carbohidratos = 6.00 # [g]
-nevarro_nummies_proteinas = 0.80 # [g]
+nevarro_nummies_grasas = 1.90 * nevarro_nummies # [g]
+nevarro_nummies_carbohidratos = 6.00 * nevarro_nummies # [g]
+nevarro_nummies_proteinas = 0.80 * nevarro_nummies # [g]
 
 # Space Soup, por porcion de 256 [ml]
-space_soup_grasas = 10.0 # [g]
-space_soup_carbohidratos = 12.0 # [g]
-space_soup_proteinas = 11.0 # [g]
+space_soup_grasas = (10.0/285) * space_soup # [g]
+space_soup_carbohidratos = (12.0/285) * space_soup # [g]
+space_soup_proteinas = (11.0/285) * space_soup # [g]
 
 # Carne de rana, por cada 100[g]
-carne_de_rana_grasas = 0.30 # [g]
-carne_de_rana_carbohidratos = 0.00 # [g]
-carne_de_ranaproteinas = 16.0 # [g]
+carne_de_rana_grasas = (0.30/100) * carne_de_rana # [g]
+carne_de_rana_carbohidratos = (0.00/100) * carne_de_rana # [g]
+carne_de_rana_proteinas = (16.0/100) * carne_de_rana # [g]
 
-"""
-Gramos a Calorias
-1[g] Grasas = 9[calorias]
-1[g] Carbohidratos = 4[calorias]
-1[g] Proteínas = 4[calorias]
-"""
+# Sumando el total de grasa
+total_grasa = round(nevarro_nummies_grasas + space_soup_grasas + carne_de_rana_grasas,2)
 
-def N_N(nevarro_nummies):
-    grasa_total_n = nevarro_nummies * nevarro_nummies_grasa
-    carbohidratos_total_n = nevarro_nummies * nevarro_nummies_carbohidratos
-    proteinas_total_n = nevarro_nummies * nevarro_nummies_proteinas
+# Sumando el total de carbohidratos
+total_carbohidratos = round(nevarro_nummies_carbohidratos + space_soup_carbohidratos + carne_de_rana_carbohidratos,2)
 
-    return grasa_total_n, carbohidratos_total_n, proteinas_total_n
+# Sumando el total de proteinas
+total_proteinas = round(nevarro_nummies_proteinas + space_soup_proteinas + carne_de_rana_proteinas,2)
 
-def S_S():
-    grasa_total_s = nevarro_nummies * nevarro_nummies_grasa
-    carbohidratos_total_s = nevarro_nummies * nevarro_nummies_carbohidratos
-    proteinas_total_s = nevarro_nummies * nevarro_nummies_proteinas
+# Calculando las calorias consumidas a partir de los datos entregados
+total_calorias = round((9*total_grasa)+(4*total_carbohidratos)+(4*total_proteinas),)
 
-    return grasa_total_s, carbohidratos_total_s, proteinas_total_s
-
-def C_R():
-    grasa_total_c = nevarro_nummies * nevarro_nummies_grasa
-    carbohidratos_total_c = nevarro_nummies * nevarro_nummies_carbohidratos
-    proteinas_total_c = nevarro_nummies * nevarro_nummies_proteinas
-
-    return grasa_total_c, carbohidratos_total_c, proteinas_total_c
+# Imprimiento en pantalla la información solicitada.
+print("Goru ha consumido:")
+print(total_grasa,"[g] de grasas")
+print(total_carbohidratos,"[g] de carbohidratos")
+print(total_proteinas,"[g] de proteinas")
+print("dando un total de")
+print(total_calorias,"[calorias]")
