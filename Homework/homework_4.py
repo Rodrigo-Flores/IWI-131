35510e71fa573fed2be7d75d3f839496a3d154b4
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-04-26 15:02:22 +0000

    Update: Adding homework_4.py and some code too.

---
+++ Homework/homework_4.py
@@ -0,0 +1,12 @@
+nombre_sucursal = input()
+coordenadas = input()
+
+carnivoro = 4000 # Plato 1
+vegetariano = 3000 # Plato 2
+vegano = 3500 # Plato 3
+
+pedido = 0
+
+while pedido != -1:
+    pedido = input()
+
