379f1330078d4c5bce52d756b5f4b02dad6e7839
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-04-30 12:30:11 -0400

    Update: Finishing extra homework multiplicacion_rusa.py

--- Extra Homework/multiplicacion_rusa.py
+++ Extra Homework/multiplicacion_rusa.py
@@ -1,18 +1,20 @@
-numero_1 = int(input("Ingrese un número: "))
-numero_2 = int(input("Ingrese otro número: "))
+a = int(input("Ingrese un número: "))
+b = int(input("Ingrese otro número: "))
 
-final = True
+suma = 0
+flag = True
 
-total = 0
+while flag:
+    if (a%2) != 0:
+        suma += b
+        b *= 2
+        
+    else:
+        b *= 2
 
-while final:
-    resultado_1 = numero_1 // 2
+    if a == 1:
+        flag = False
 
-    if resultado_1 % 2 != 0:
-        total += numero_2
-        numero_2 *= 2
+    a = a//2
 
-    elif resultado_1 == 1:
-        final = False
-
-print("Total:", total)
+print("Total:", suma)
