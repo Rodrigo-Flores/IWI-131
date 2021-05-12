d381414d86df66d1252874bd332219eec3cb87d8
Nao Motoki <nao@localhost.localdomain>
2021-05-12 02:02:11 +0000

    Update: Continuing with homework.py

--- Homework/homewrok_6.py
+++ Homework/homewrok_6.py
@@ -1,27 +1,33 @@
-frase = input("Ingrese texto: ")
-significado = input("Ingrese significado: ")
+# frase = input("Ingrese texto: ")
+# significado = input("Ingrese significado: ")
 
+frase = "Hola <3, cómo estás? :-D. Voy atrasado a la clase >.<"
+significado = "<3*corazon$:-D*cara feliz$>.<*incomodo$"
 
-emoji = ""
-traduccion = ""
+i = 0
 
-for i in significado:
-    if i == "*":
+a = 0
+
+def extraccion():
+    if significado[i] == "*":
         emoji = significado[:i]
-        temp = i +1
+        t = i + 1
 
-    elif i == "$":
-        traduccion = significado[temp:i]
+    elif significado[i] == "$":
+        significado_emoji = significado[t:i].upper()
+        significado = significado[i+1:]    
+        
+    i+=1
 
-    for n in frase:
-        if n == emoji:
-            
+    
 
 
 
-'''
+
+
+"""
 Ingrese texto: Hola <3, cómo estás? :-D. Voy atrasado a la clase >.<
 Ingrese significados:<3*corazon$:-D*cara feliz$>.<*incomodo$
 Texto traducido: Hola CORAZON, cómo estás? CARA FELIZ. Voy atrasado a
 la clase INCOMODO
-'''
+"""
